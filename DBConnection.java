97e4f6948e9f762fe04fe250fb7d244eec71b830
LuckySaini-Java-developer <lucky16052006@gmail.com>
2026-06-08 19:12:05 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -8,10 +8,9 @@ public class DBConnection {
         try {
             if (con == null || con.isClosed()) {
                 // क्लाउड एनवायरनमेंट या डिफ़ॉल्ट Neon क्रेडेंशियल्स उठाना
-                String dbHost = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "ep-shiny-cell-aqt8ekq0.us-east-1.aws.neon.tech";
-                String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "neondb_owner";
-                String dbPass = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "npg_Gtightwla4brC6@ep";
-
+               String dbHost = System.getenv("DB_HOST");
+                String dbUser = System.getenv("DB_USER");
+                String dbPass = System.getenv("DB_PASSWORD");
                 String url = "jdbc:postgresql://" + dbHost + ":5432/neondb?sslmode=require";
                 
                 // PostgreSQL ड्राइवर लोड करना
