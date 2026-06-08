402462dbe063cb3da7ff4a22d7a223912eb00d0d
LuckySaini-Java-developer <lucky16052006@gmail.com>
2026-06-08 22:50:08 +0530

    Update DBConnection.java

--- WEB-INF/classes/DBConnection.java
+++ WEB-INF/classes/DBConnection.java
@@ -1,4 +1,3 @@
-package WEB-INF.classes; // फोल्डर के अनुसार पैकेज नाम देना ज़रूरी है
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -8,12 +7,14 @@ public class DBConnection {
     public static Connection getConnection() {
         try {
             if (con == null || con.isClosed()) {
+                // रेंडर की सुरक्षित तिजोरी से क्रेडेंशियल्स उठाना
                 String dbHost = System.getenv("DB_HOST");
                 String dbUser = System.getenv("DB_USER");
                 String dbPass = System.getenv("DB_PASSWORD");
 
                 String url = "jdbc:postgresql://" + dbHost + ":5432/neondb?sslmode=require";
                 
+                // PostgreSQL ड्राइवर लोड करना
                 Class.forName("org.postgresql.Driver");
                 con = DriverManager.getConnection(url, dbUser, dbPass);
             }
